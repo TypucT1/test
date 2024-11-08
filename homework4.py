462b440bafd44ec1e2a474cd3fccdbe262034ab7
TypucT1 <smoke67smok@gmail.com>
2024-11-08 13:54:08 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+immutable_var =( 1, 2, 'a', 'b')
+print(immutable_var)
+#immutable_var[3] = 15          содержимое кортежа не изменяемо(за исключением списка внутри кортежа)
+#print(immutable_var)
+mutable_list = [1, 2, 'a', 'b', 'Modified']
+print(mutable_list)
+mutable_list[1] = 35
+print(mutable_list)
