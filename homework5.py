41a14c1e44c1ad0a83e70ebdd1bedf7ae2d3a39b
TypucT1 <smoke67smok@gmail.com>
2024-11-08 14:39:56 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple','banana' , 'orange' , 'kiwi','papaya']
+print(my_list)
+print(my_list[0],my_list[-1])
+print(my_list[2 : 5])
+my_list[2]= 'mango'
+print(my_list)
+
+my_dict = {'Mark' : 89902002020, 'Luka' : 89993003030, 'Rita' : 89111001010}
+print(my_dict)
+print(my_dict['Luka'])
+my_dict['Mark'] = 89999009090
+print(my_dict)
