6490a440ba7c4913c0d3517ce7f5e3dd496db96b
TypucT1 <smoke67smok@gmail.com>
2024-11-08 13:57:11 +0300

    homework4.py

--- homework4.py
+++ homework4.py
@@ -1,6 +1,6 @@
 immutable_var =( 1, 2, 'a', 'b')
 print(immutable_var)
-#immutable_var[3] = 15          содержимое кортежа не изменяемо(за исключением списка внутри кортежа)
+#immutable_var[3] = 15          содержимое кортежа неизменяемый объект(за исключением списка внутри кортежа)
 #print(immutable_var)
 mutable_list = [1, 2, 'a', 'b', 'Modified']
 print(mutable_list)
